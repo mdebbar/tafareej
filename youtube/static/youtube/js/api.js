fe4b0167e1866102a7f2278bc90980e6b87639c1
Mouad Debbar <mouad.debbar@gmail.com>
2014-02-09 17:51:59 -0800

    Refactor the JS API to remove duplicate code.

--- youtube/static/youtube/js/api.js
+++ youtube/static/youtube/js/api.js
@@ -13,50 +13,61 @@
     callback(response.items);
   }
 
-  function paginator(query, apiMethod) {
+  function paginator(apiMethod, apiArg) {
     return function(response) {
       var pageToken = response.nextPageToken;
       this.next = function(callback) {
         if (!pageToken) {
           return false;
         }
-        return API[apiMethod](query, pageToken, callback);
+        return API[apiMethod](apiArg, pageToken, callback);
       };
     };
   }
 
   global.API = {
-    // TODO: refactor search() <=> searchPage() AND related() <=> relatedPage()
-    search: function(query, callback) {
+    /**
+     * search(query, [pageToken], callback)
+     */
+    search: function(query, pageToken, callback) {
+      if (typeof pageToken === 'function') {
+        callback = pageToken;
+        pageToken = null;
+      }
       query = cleanQuery(query);
-      console.log('Searching for:', query);
       this._search && this._search.abandon();
-      return this._search = new X(URL.API.search(query))
-        .success(paginator(query, 'searchPage'))
+      var url;
+      if (pageToken == null) {
+        console.log('Searching for:', query);
+        url = URL.API.search(query);
+      } else {
+        console.log('Search page [' + pageToken + '] for:', query);
+        url = URL.API.searchPage(query, pageToken);
+      }
+      return this._search = new X(url)
+        .success(paginator('search', query))
         .success(resultsCallback.bind(null, callback))
         .error(errorHandler);
     },
-    searchPage: function(query, pageToken, callback) {
-      console.log('Search page [' + pageToken + '] for:', query);
-      this._search && this._search.abandon();
-      return this._search = new X(URL.API.searchPage(query, pageToken))
-        .success(paginator(query, 'searchPage'))
-        .success(resultsCallback.bind(null, callback))
-        .error(errorHandler);
-    },
-    related: function(videoID, callback) {
-      console.log('Related videos for:', videoID);
+    /**
+     * related(videoID, [pageToken], callback)
+     */
+    related: function(videoID, pageToken, callback) {
+      if (typeof pageToken === 'function') {
+        callback = pageToken;
+        pageToken = null;
+      }
       this._related && this._related.abandon();
-      return this._related = new X(URL.API.related(videoID))
-        .success(paginator(videoID, 'relatedPage'))
-        .success(resultsCallback.bind(null, callback))
-        .error(errorHandler);
-    },
-    relatedPage: function(videoID, pageToken, callback) {
-      console.log('Related videos page[' + pageToken + '] for:', videoID);
-      this._related && this._related.abandon();
-      return this._related = new X(URL.API.relatedPage(videoID, pageToken))
-        .success(paginator(videoID, 'relatedPage'))
+      var url;
+      if (pageToken == null) {
+        console.log('Related videos for:', videoID);
+        url = URL.API.related(videoID);
+      } else {
+        console.log('Related videos page[' + pageToken + '] for:', videoID);
+        url = URL.API.relatedPage(videoID, pageToken);
+      }
+      return this._related = new X(url)
+        .success(paginator('related', videoID))
         .success(resultsCallback.bind(null, callback))
         .error(errorHandler);
     },
