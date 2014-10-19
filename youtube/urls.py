37e7102557dc7ae896f273b3922d6f1dc77e7b3d
Mouad Debbar <mouad.debbar@gmail.com>
2014-10-19 10:48:02 -0700

    Remove Ajax APIs from Django since they are now in CherryPy

--- youtube/urls.py
+++ youtube/urls.py
@@ -6,19 +6,3 @@ urlpatterns = patterns('',
   url(r'^view/(?P<video_id>[-_\w]+)/$', 'youtube.views.view'),
   url(r'^(?P<video_id>[-_\w]+)/$', 'youtube.views.view', name='view_video'),
 )
-
-# JSON API
-urlpatterns += patterns('',
-  url(r'^api/search/(?P<query>.+)/page/(?P<page_token>.+)/$', 'youtube.views.api_search'),
-  url(r'^api/search/(?P<query>.+)/$', 'youtube.views.api_search'),
-
-  url(r'^api/related/(?P<video_id>[-_\w]+)/page/(?P<page_token>.+)/$', 'youtube.views.api_related'),
-  url(r'^api/related/(?P<video_id>[-_\w]+)/$', 'youtube.views.api_related'),
-
-  url(r'^api/popular/page/(?P<page_token>.+)/$', 'youtube.views.api_popular'),
-  url(r'^api/popular/$', 'youtube.views.api_popular'),
-
-  url(r'^api/autocomplete/(?P<query>.+)/$', 'youtube.views.api_autocomplete'),
-
-  url(r'^api/(?P<video_id>[-_\w]+)/$', 'youtube.views.api_one'),
-)
--- youtube/views.py
+++ youtube/views.py
@@ -1,30 +1,8 @@
 from django.conf import settings
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404
 from django.shortcuts import render_to_response
-from tafareej import JsonResponse
 from youtube import api
 
-
-def api_search(req, query, page_token=None):
-  response = api.searchWithDetails(query, page_token)
-  response['items'] = tuple(video.dict() for video in response['items'])
-  return JsonResponse(response)
-
-def api_related(req, video_id, page_token=None):
-  response = api.related(video_id, page_token)
-  response['items'] = tuple(video.dict() for video in response['items'])
-  return JsonResponse(response)
-
-def api_popular(req, page_token=None):
-  response = api.popular(page_token)
-  response['items'] = tuple(video.dict() for video in response['items'])
-  return JsonResponse(response)
-
-def api_one(req, video_id):
-  return JsonResponse(api.one_video(video_id).dict())
-
-def api_autocomplete(req, query):
-  return HttpResponse(api.autocomplete(query, **req.GET.dict()), content_type='text/javascript')
 
 def view(req, video_id):
   video = api.one_video(video_id)
