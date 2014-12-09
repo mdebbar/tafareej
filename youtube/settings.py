e0a3ea17ce2b7e9d230970b93ca5ba1fea7e1429
Mouad Debbar <mouad.debbar@gmail.com>
2014-12-09 02:34:18 -0800

    Clean up the old Django stuff

--- youtube/settings.py
+++
@@ -1,7 +0,0 @@
-GOOGLE_API_KEY = 'AIzaSyBDhbq80CkRXdbP3wvT17su77zo-gzeKRM'
-
-DEFAULT_OPTIONS = {
-  'details': {
-    'part': 'id,snippet,contentDetails,statistics',
-  },
-}
--- youtube/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-  url(r'^$', 'youtube.views.home'),
-
-  url(r'^view/(?P<video_id>[-_\w]+)/$', 'youtube.views.view'),
-  url(r'^(?P<video_id>[-_\w]+)/$', 'youtube.views.view', name='view_video'),
-)
