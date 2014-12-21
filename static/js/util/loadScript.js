c91198cbe9baf3ae2d933d35e7375a877070333a
Mouad Debbar <mouad.debbar@gmail.com>
2014-12-20 18:59:22 -0800

    Add an optional callback to loadScript()

--- static/js/util/loadScript.js
+++ static/js/util/loadScript.js
@@ -1,6 +1,9 @@
-module.exports = function(url) {
+module.exports = function(url, callback) {
   var tag = document.createElement('script');
   tag.src = url;
+  if (callback) {
+    tag.onload = callback;
+  }
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 };
